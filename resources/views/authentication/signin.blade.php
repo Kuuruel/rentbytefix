5763aebe237b457ed64753797123093ce382301a
YudhiSatria <179100971+YudhiSatria@user.noreply.github.com>
2025-08-29 10:47:26 +0800

    login,regis

--- resources/views/authentication/signin.blade.php
+++ resources/views/authentication/signin.blade.php
@@ -1,10 +1,10 @@
-<!-- meta tags and other links -->
+<!-- resources\views\authentication\signin.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 
-<x-head/>
+<x-head />
 
-<body class="dark:bg-neutral-800 bg-neutral-100 dark:text-white"> 
+<body class="dark:bg-neutral-800 bg-neutral-100 dark:text-white">
 
     <section class="bg-white dark:bg-dark-2 flex flex-wrap min-h-[100vh]">
         <div class="lg:w-1/2 lg:block hidden">
@@ -21,50 +21,66 @@
                     <h4 class="mb-1">Sign In to your Account</h4>
                     <p class="mb-8 text-secondary-light text-lg">Welcome back! Please enter your detail</p>
                 </div>
-                <form action="{{route('signin')}}" method="POST">
-                        @csrf
+                <form action="{{ route('signin') }}" method="POST">
+                    @csrf
                     <div class="icon-field mb-4 relative">
                         <span class="absolute start-4 top-1/2 -translate-y-1/2 pointer-events-none flex text-xl">
                             <iconify-icon icon="mage:email"></iconify-icon>
                         </span>
-                        <input type="email" name="email" class="form-control h-[56px] ps-11 border-neutral-300 bg-neutral-50 dark:bg-dark-2 rounded-xl" placeholder="Email" required>
+                        <input type="email" name="email"
+                            class="form-control h-[56px] ps-11 border-neutral-300 bg-neutral-50 dark:bg-dark-2 rounded-xl"
+                            placeholder="Email" required>
                     </div>
                     <div class="relative mb-5">
                         <div class="icon-field">
                             <span class="absolute start-4 top-1/2 -translate-y-1/2 pointer-events-none flex text-xl">
                                 <iconify-icon icon="solar:lock-password-outline"></iconify-icon>
                             </span>
-                            <input type="password" name="password" class="form-control h-[56px] ps-11 border-neutral-300 bg-neutral-50 dark:bg-dark-2 rounded-xl" id="your-password" placeholder="Password" required>
+                            <input type="password" name="password"
+                                class="form-control h-[56px] ps-11 border-neutral-300 bg-neutral-50 dark:bg-dark-2 rounded-xl"
+                                id="your-password" placeholder="Password" required>
                         </div>
-                        <span class="toggle-password ri-eye-line cursor-pointer absolute end-0 top-1/2 -translate-y-1/2 me-4 text-secondary-light" data-toggle="#your-password"></span>
+                        <span
+                            class="toggle-password ri-eye-line cursor-pointer absolute end-0 top-1/2 -translate-y-1/2 me-4 text-secondary-light"
+                            data-toggle="#your-password"></span>
                     </div>
                     <div class="mt-7">
                         <div class="flex justify-between gap-2">
                             <div class="flex items-center">
-                                <input class="form-check-input border border-neutral-300" type="checkbox" value="" id="remeber">
+                                <input class="form-check-input border border-neutral-300" type="checkbox" value=""
+                                    id="remeber">
                                 <label class="ps-2" for="remeber">Remember me </label>
                             </div>
-                            <a href="javascript:void(0)" class="text-primary-600 font-medium hover:underline">Forgot Password?</a>
+                            <a href="javascript:void(0)" class="text-primary-600 font-medium hover:underline">Forgot
+                                Password?</a>
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-primary justify-center text-sm btn-sm px-3 py-4 w-full rounded-xl mt-8"> Sign In</button>
+                    <button type="submit"
+                        class="btn btn-primary justify-center text-sm btn-sm px-3 py-4 w-full rounded-xl mt-8"> Sign
+                        In</button>
 
-                    <div class="mt-8 center-border-horizontal text-center relative before:absolute before:w-full before:h-[1px] before:top-1/2 before:-translate-y-1/2 before:bg-neutral-300 before:start-0">
+                    <div
+                        class="mt-8 center-border-horizontal text-center relative before:absolute before:w-full before:h-[1px] before:top-1/2 before:-translate-y-1/2 before:bg-neutral-300 before:start-0">
                         <span class="bg-white dark:bg-dark-2 z-[2] relative px-4">Or sign in with</span>
                     </div>
                     <div class="mt-8 flex items-center gap-3">
-                        <button type="button" class="font-semibold text-neutral-600 dark:text-neutral-200 py-4 px-6 w-1/2 border rounded-xl text-base flex items-center justify-center gap-3 line-height-1 hover:bg-primary-50">
-                            <iconify-icon icon="ic:baseline-facebook" class="text-primary-600 text-xl line-height-1"></iconify-icon>
+                        <button type="button"
+                            class="font-semibold text-neutral-600 dark:text-neutral-200 py-4 px-6 w-1/2 border rounded-xl text-base flex items-center justify-center gap-3 line-height-1 hover:bg-primary-50">
+                            <iconify-icon icon="ic:baseline-facebook"
+                                class="text-primary-600 text-xl line-height-1"></iconify-icon>
                             Google
                         </button>
-                        <button type="button" class="font-semibold text-neutral-600 dark:text-neutral-200 py-4 px-6 w-1/2 border rounded-xl text-base flex items-center justify-center gap-3 line-height-1 hover:bg-primary-50">
-                            <iconify-icon icon="logos:google-icon" class="text-primary-600 text-xl line-height-1"></iconify-icon>
+                        <button type="button"
+                            class="font-semibold text-neutral-600 dark:text-neutral-200 py-4 px-6 w-1/2 border rounded-xl text-base flex items-center justify-center gap-3 line-height-1 hover:bg-primary-50">
+                            <iconify-icon icon="logos:google-icon"
+                                class="text-primary-600 text-xl line-height-1"></iconify-icon>
                             Google
                         </button>
                     </div>
                     <div class="mt-8 text-center text-sm">
-                        <p class="mb-0">Don't have an account? <a href="{{ route('signup') }}" class="text-primary-600 font-semibold hover:underline">Sign Up</a></p>
+                        <p class="mb-0">Don't have an account? <a href="{{ route('signup') }}"
+                                class="text-primary-600 font-semibold hover:underline">Sign Up</a></p>
                     </div>
 
                 </form>
@@ -72,29 +88,30 @@
         </div>
     </section>
 
-<script>
-  // ================== Password Show Hide Js Start ==========
-  function initializePasswordToggle(toggleSelector) {
-    const toggles = document.querySelectorAll(toggleSelector);
+    <script>
+        // ================== Password Show Hide Js Start ==========
+        function initializePasswordToggle(toggleSelector) {
+            const toggles = document.querySelectorAll(toggleSelector);
 
-    toggles.forEach(function (toggle) {
-      toggle.addEventListener("click", function () {
-        this.classList.toggle("ri-eye-off-line");
-        const input = document.querySelector(this.getAttribute("data-toggle"));
+            toggles.forEach(function(toggle) {
+                toggle.addEventListener("click", function() {
+                    this.classList.toggle("ri-eye-off-line");
+                    const input = document.querySelector(this.getAttribute("data-toggle"));
 
-        if (input) {
-          input.type = input.type === "password" ? "text" : "password";
+                    if (input) {
+                        input.type = input.type === "password" ? "text" : "password";
+                    }
+                });
+            });
         }
-      });
-    });
-  }
 
-  // Call the function
-  initializePasswordToggle(".toggle-password");
-  // ========================= Password Show Hide Js End ===========================
-</script>
-                    
+        // Call the function
+        initializePasswordToggle(".toggle-password");
+        // ========================= Password Show Hide Js End ===========================
+    </script>
+
     <x-script />
 
 </body>
+
 </html>
